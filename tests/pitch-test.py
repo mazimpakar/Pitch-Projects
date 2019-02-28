b519e1a7a38f5cf8aef3daab4ea3d2042de6939b
mazimpakar <rosemazimpaka2@gmail.com>
2019-02-28 17:12:56 +0200

    add comment class

--- tests/pitch-test.py
+++ tests/pitch-test.py
@@ -1,17 +1,15 @@
 import unittest
-from app.models import Pitch  
-Pitch = Pitch
+from app.models import Pitch
 
-class PitchTest(unittest.TestCase):
-    '''
-    Test Class to test the behaviour of the Pitch class
-    '''
+class PitchModelTest(unittest.TestCase):
 
     def setUp(self):
-        '''
-        Set up method that will run before every Test
-        '''
-        self.new_pitch = Pitch(133,'killed by excelence')
+        self.new_pitch = Pitch(id = 1, title = 'What Makes a Song Sad', pitch_content = 'Where does sad music get its sadness from? And whom should you ask—a composer or a cognitive psychologist?', category = 'Pickup Lines', upvote = 10, downvote = 12, author = 'DANIEL WATTENBERG'
+)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_pitch,Pitch))
+        self.assertTrue(isinstance(self.new_pitch, Pitch))
+
+    def test_save_comment(self):
+        self.new_comment.save_comment()
+        self.assertTrue(len(Comment.query.all())>0)
